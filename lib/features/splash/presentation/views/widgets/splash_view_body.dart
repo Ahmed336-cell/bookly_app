0322ca44aeb905f324758051625d8ccf19d56223
Ahmed-Maher <ahmaher04@gmail.com>
2024-01-31 20:23:17 +0200

    Initial commit

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class SplashBodyView extends StatelessWidget {
+  const SplashBodyView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+    );
+  }
+}
