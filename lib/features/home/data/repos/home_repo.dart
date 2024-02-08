78cda7720abf8d0fb161886a9c158de63443041a
Ahmed-Maher <ahmaher04@gmail.com>
2024-02-08 18:32:33 +0200

    create server failure from dio error

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -2,7 +2,7 @@ import 'package:bookly/core/errors/failuers.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class HomeRepo{
-  Future<Either<Faliuer,List<BookModel>>> fetchNewsedBooks();
-  Future<Either<Faliuer,List<BookModel>>> fetchFeaturedBooks();
+abstract class HomeRepo {
+  Future<Either<Faliuer, List<BookModel>>> fetchNewsedBooks();
+  Future<Either<Faliuer, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/features/home/data/repos/home_repo_imp.dart
+++ lib/features/home/data/repos/home_repo_imp.dart
@@ -3,26 +3,30 @@ import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
-class HomeRepoImo implements HomeRepo{
+class HomeRepoImo implements HomeRepo {
   ApiService apiService;
 
   HomeRepoImo(this.apiService);
 
   @override
-  Future<Either<Faliuer, List<BookModel>>> fetchNewsedBooks() async{
-
-    try{
-      var data = await apiService.get(endpoint: "volumes?Filtering=free-ebooks&q=subject:Programming&sorting=newsed");
-      List <BookModel>books = [];
-      for(var items in data['items']){
+  Future<Either<Faliuer, List<BookModel>>> fetchNewsedBooks() async {
+    try {
+      var data = await apiService.get(
+          endpoint:
+              "volumes?Filtering=free-ebooks&q=subject:Programming&sorting=newsed");
+      List<BookModel> books = [];
+      for (var items in data['items']) {
         books.add(BookModel.fromJson(items));
       }
 
       return right(books);
-
-    }on Exception catch(e){
-      return Left(ServerFailuer());
+    } on Exception catch (e) {
+      if (e is DioError) {
+        return Left(ServerFailuer.fromDioError(e));
+      }
+      return left(ServerFailuer(e.toString()));
     }
   }
 
@@ -30,5 +34,4 @@ class HomeRepoImo implements HomeRepo{
   Future<Either<Faliuer, List<BookModel>>> fetchFeaturedBooks() {
     throw UnimplementedError();
   }
-
 }
